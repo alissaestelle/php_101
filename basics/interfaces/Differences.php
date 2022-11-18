7b78c83976def0e78ad032363008fde4b529b0fe
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-11-18 13:06:30 -0500

    Refactored Basics Directory

--- basics/interfaces/Differences.php
+++ basics/interfaces/Differences.php
@@ -4,12 +4,80 @@ interface Provider {
     function authorize();
 }
 
-class GitHub {
+// class User {
+//     function __construct(public Provider $provider)
+//     {}
+
+//     function login()
+//     {
+//         return $this->provider->authorize();
+//     }
+// }
+
+// $alissa = new User(new GitHub);
+// var_dump($alissa->login());
+
+class User {
+    function __construct(public $name)
+    {}
+
     function login(Provider $provider)
     {
-        $provider->authorize();
+        return $provider->authorize();
     }
-
 }
+
+// Note: PHP supports multiple interface implementation.
+
+class GitHub implements Provider {
+    function __construct(public $name)
+    {}
+
+    function authorize()
+    {
+        // var_dump('GitHub User Authorized!');
+        return "$this->name User Authorized!";
+    }
+}
+
+class Twitter implements Provider {
+    function __construct(public $name)
+    {}
+
+    function authorize()
+    {
+        // var_dump('GitHub User Authorized!');
+        return "$this->name User Authorized!";
+    }
+}
+
+$alissa = new User('Alissa');
+$github = new GitHub('GitHub');
+$twitter = new Twitter('Twitter');
+
+// This login() fx accepts interface implemented classes as an argument:
+var_dump($alissa->login($github));
+var_dump($alissa->login($twitter));
+
+
+// ABSTRACT COMPARISON
+
+abstract class Service {
+    abstract protected function authorize(); 
+}
+
+// Note: PHP doesn't support multiple inheritance.
+
+class Facebook extends Service {
+    function __construct(protected $name){}
+    function authorize()
+    {
+        return "$this->name User Authorized!";
+    }
+}
+
+$facebook = new Facebook('Facebook');
+var_dump($facebook->authorize());
+
 
 ?>
